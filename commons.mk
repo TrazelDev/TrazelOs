643e610e5b5c8cd173eaaba940a5cbedcb89e8ec
TrazelDev <nirmot327@gmail.com>
2025-03-06 08:38:37 +0200

    (chore): Removed whitespaces and an unnecessary /

--- commons.mk
+++ commons.mk
@@ -4,8 +4,7 @@ export ASM = nasm
 
 
 # path of this directory:
-CURR_DIR := $(dir $(lastword $(MAKEFILE_LIST))) 
+CURR_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 CURR_DIR := $(strip $(CURR_DIR))
 
-export BIN_DIR := $(CURR_DIR)bin/
-
+export BIN_DIR := $(CURR_DIR)bin
