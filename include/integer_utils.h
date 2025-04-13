c591aa7e95ba272c4dc0bb1f8640f05ab680afb0
TrazelDev <nirmot327@gmail.com>
2025-04-13 17:38:43 +0300

    (refactor)[utilities/integer_utils.h]: Reimplemented the functions (itoa_unsigned, itoa_signed) Some further testing needs to be done

--- include/integer_utils.h
+++ include/integer_utils.h
@@ -6,9 +6,9 @@
  * The bases that are supported
  */
 enum Base {
-	HEX = 1,
-	DECIMAL,
-	OCTAL,
+	HEX = 0,
+	DECIMAL = 1,
+	OCTAL = 2,
 };
 
 
@@ -17,7 +17,7 @@ enum Base {
  * value - the int that needs to be converted into a string
  * buffer - the buffer that the strings needs to be loaded to
  * base - the base it needs to be converted into
- * Return value - if the buffer is big enough then it returns the buffer address else it returns a NULL
+ * Return value - the inputed buffer address
  */
 char* itoa_unsigned(uint64_t value, char* buffer, enum Base base);
 char* itoa_signed(int64_t value, char* buffer, enum Base base);
