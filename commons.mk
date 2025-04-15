2b946eac7bb7f349ce66bbd3653be7d38999ca67
TrazelDev <nirmot327@gmail.com>
2025-04-15 18:00:41 +0300

    (fix)[commons.mk]: fixed a problem with detecting the current directory

--- commons.mk
+++ commons.mk
@@ -1,7 +1,5 @@
 # Path of this directory:
-CURR_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-CURR_DIR := $(strip $(CURR_DIR))
-unexport CURR_DIR
+CURR_DIR != pwd
 
 # Tools:
 export ASM = nasm
@@ -9,8 +7,8 @@ export CC = x86_64-elf-gcc
 export LD = x86_64-elf-ld
 
 # compiler global setting:
-export GLOBAL_INCLUDE_DIR := $(CURR_DIR)include
-export DRIVERS_DIR := $(CURR_DIR)drivers
+export GLOBAL_INCLUDE_DIR := $(CURR_DIR)/include
+export DRIVERS_DIR := $(CURR_DIR)/drivers
 
 # Common compiler flags:
 # ------------------------------------------------------------------
@@ -22,7 +20,7 @@ export CC_FLAGS := -ffreestanding -mno-red-zone -m64 -I $(GLOBAL_INCLUDE_DIR)
 
 
 # binnaries path
-export BIN_DIR := $(CURR_DIR)bin
+export BIN_DIR := $(CURR_DIR)/bin
 export MBR_BIN := $(BIN_DIR)/mbr.bin
 export BOOTLOADER_BIN := $(BIN_DIR)/bootloader.bin
 export DRIVERS_BIN_DIR := $(BIN_DIR)/drivers
