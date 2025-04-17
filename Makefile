99423c3c4ba76a461f2665dff941fce0a9cd714e
TrazelDev <nirmot327@gmail.com>
2025-04-18 02:42:40 +0300

    (refactor)[bootloader makefile]: Updated the bootloader makefile Updated the bootloader makefile and the main Makefile so it will compile the project in a way that fits the previous commit

--- Makefile
+++ Makefile
@@ -9,12 +9,15 @@ clean:
 	rm -rf bin
 
 
-$(OS_IMG): $(BIN_MBR)
+$(OS_IMG): $(BIN_MBR) $(BIN_BOOTLOADER)
 	dd if=/dev/zero of=$(OS_IMG) count=100
 	dd if=$(BIN_MBR) of=$(OS_IMG) count=1 conv=notrunc
-# dd if=$(BIN_BOOTLOADER) of=$(OS_IMG) seek=1 conv=notrunc
+	dd if=$(BIN_BOOTLOADER) of=$(OS_IMG) seek=1 conv=notrunc
 	echo ', 5, L, *' | sfdisk $(OS_IMG)
 
 
 $(BIN_MBR):
-	$(MAKE) -C $(DIR_MBR)
+	$(MAKE) $(BIN_MBR) -C $(DIR_MBR)
+
+$(BIN_BOOTLOADER):
+	$(MAKE) $(BIN_BOOTLOADER) -C $(DIR_BOOTLOADER)
--- commons.mk
+++ commons.mk
@@ -1,20 +1,8 @@
 DIR_CURR != pwd
 
-# tools:
-export ASM = nasm
-export CC = x86_64-elf-gcc
-export LD = x86_64-elf-ld
-
-# common compiler flags:
-# ------------------------------------------------------------------
-# -ffreestanding - says that there are no stanard libs
-# -mno-red-zone - something with stack just in gcc, man it
-# -m64 - size of values like int long and so on
-export CC_FLAGS := -ffreestanding -mno-red-zone -m64 -I $(GLOBAL_INCLUDE_DIR)
-# ------------------------------------------------------------------
-
 # important directories:
 export DIR_MBR := $(DIR_CURR)/boot/mbr
+export DIR_BOOTLOADER := $(DIR_CURR)/boot/bootloader
 export DIR_DRIVERS := $(DIR_CURR)/drivers
 export DIR_INCLUDS := $(DIR_CURR)/include
 
@@ -27,3 +15,18 @@ export DIR_BIN_UTILS := $(DIR_BIN)/utils
 export OS_IMG := $(DIR_BIN)/os.img
 export BIN_MBR := $(DIR_BIN)/mbr.bin
 export BIN_BOOTLOADER := $(DIR_BIN)/bootloader.bin
+
+
+
+# tools:
+export ASM = nasm
+export CC = x86_64-elf-gcc
+export LD = x86_64-elf-ld
+
+# common compiler flags:
+# ------------------------------------------------------------------
+# -ffreestanding - says that there are no stanard libs
+# -mno-red-zone - something with stack just in gcc, man it
+# -m64 - size of values like int long and so on
+export CC_FLAGS := -ffreestanding -mno-red-zone -m64 -I $(DIR_INCLUDS)
+# ------------------------------------------------------------------
