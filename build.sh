936f39498e171753d8129d7872d2d364bad0b1c0
TrazelDev <nirmot327@gmail.com>
2025-08-22 20:12:25 +0300

    (feature)[build.sh]: Made it so it will run bash instead of sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ xhost +local:root
 docker run -it -e DISPLAY=$DISPLAY -e XDG_RUNTIME_DIR=/tmp/runtime-root \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v $(pwd):/mnt/TrazelOs \
-	os-build-env:latest sh -c "umask 0000 && exec sh"
+	os-build-env:latest sh -c "umask 0000 && exec bash"
