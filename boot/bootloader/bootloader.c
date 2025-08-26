e3478152b70b45c7e9223f014540f07e7eb0f394
Nir Kriemer <nirmot327@gmail.com>
2025-08-26 17:05:03 +0300

    (feature)[bootloader/partion_table]: Loaded partion table into struct
    * Created a function to load the mbr partion table into a struct
    * Created a function to print the mbr partion table
    * Fixed an issue with itoa function inside of integer_utils in libc_tz

--- boot/bootloader/bootloader.c
+++ boot/bootloader/bootloader.c
@@ -1,16 +1,19 @@
 #include <drivers/vga_text.h>
 #include <drivers/ata_pio.h>
+#include <integer_utils.h>
+#include <strings.h>
 #include <types.h>
-
+#include "partion_table.h"
 
 void bootloader_entry() {
 	set_cursor_position(position_to_cordinates(0, 0));
 	print_string("Bootloader stage 2\n");
-
-	ata_pio_init();
-	// WARNING: This test breaks the bootsector, so run the os and than rebuild it
-	// after so it will run
-	if (ata_pio_min_test()) print_string("Test successful");
 	
+	ata_pio_init();
+	
+	// Getting a partion table:
+	struct mbr_partion_table mbr_table = get_drive_mbr_partion_table();
+	print_mbr_partion_table(mbr_table);
+
 	asm volatile("hlt");
 }
