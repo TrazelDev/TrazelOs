1f83c500a939b20ae9667f0ad5831305dcf7de1b
Nir Kriemer <nirmot327@gmail.com>
2026-03-28 22:08:57 +0300

    (feature)[pmm]: Implement functions to allocate pages and return them.

--- kernel/mm/pmm/pmm_bitmap.c
+++ kernel/mm/pmm/pmm_bitmap.c
@@ -51,9 +51,62 @@ void init_pmm(volatile struct limine_memmap_response* memmap_response,
 	printk("initialized PMM Bitmap! Bitmap located at: 0x%x\n", s_bitmap.bitmap);
 }
 
+void* pmm_alloc_page(void) {
+	for (uint64_t i = 0; i < s_bitmap.bit_count; i++) {
+		if (bitmap_get_bit_value(&s_bitmap, i)) {
+			continue;
+		}
+
+		void* page_addr = (void*)(i * PAGE_SIZE);
+		set_page_as_used(page_addr);
+		return page_addr;
+	}
+
+	return NULL;
+}
+void* pmm_alloc_pages(size_t count) {
+	KERNEL_ASSERT(count != 0, "PMM can't allocate 0 pages");
+
+	bool found_range = true;
+	for (uint64_t i = 0; i <= s_bitmap.bit_count - count; i++) {
+		found_range = true;
+		for (uint64_t j = i; j < i + count; j++) {
+			if (bitmap_get_bit_value(&s_bitmap, j)) {
+				found_range = false;
+				i = j;
+				break;
+			}
+		}
+		if (!found_range) {
+			continue;
+		}
+
+		for (uint64_t j = i; j < i + count; j++) {
+			set_page_as_used((void*)(j * PAGE_SIZE));
+		}
+		return (void*)(i * PAGE_SIZE);
+	}
+
+	return NULL;
+}
+
+void pmm_free_page(void* physical_addr) { set_page_as_unused(physical_addr); }
+void pmm_free_pages(void* physical_addr, size_t count) {
+	for (uint64_t i = 0; i < count; i++) {
+		set_page_as_unused((void*)((uint64_t)physical_addr + (i * PAGE_SIZE)));
+	}
+}
+
 static inline size_t get_ram_max_size(volatile struct limine_memmap_response* memmap_response) {
-	return memmap_response->entries[memmap_response->entry_count - 1]->base +
-		   memmap_response->entries[memmap_response->entry_count - 1]->length;
+	size_t max_address = 0;
+	for (uint32_t i = 0; i < memmap_response->entry_count; i++) {
+		size_t top = memmap_response->entries[i]->base + memmap_response->entries[i]->length;
+		if (top > max_address) {
+			max_address = top;
+		}
+	}
+
+	return max_address;
 }
 
 static inline int64_t get_bitmap_region(volatile struct limine_memmap_response* memmap_response,
