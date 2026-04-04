89d15af8f9a558db0411cb035c6fa01bbb439b15
Nir Kriemer <nirmot327@gmail.com>
2026-04-04 22:29:09 +0300

    (feature)[pmm]: Add functions to get the hhdm addr to instantly use.
    * Previously I returned the physical addr in the allocate page in the pmm,
    but this causes an issue where I need to always add hhdm offset to the addr,
    so I created a wrapper functions to do it for me.

--- kernel/include/pmm.h
+++ kernel/include/pmm.h
@@ -1,11 +1,19 @@
 #pragma once
 #include <include/vendor/limine.h>
 
+#define REGULAR_PAGE_SIZE 0x1000
 void init_pmm(volatile struct limine_memmap_response* memmap_response,
 			  volatile struct limine_hhdm_response* hhdm_response);
 
 void* pmm_alloc_page(void);
 void* pmm_alloc_pages(size_t count);
+
+/** Allocates a physical page returns the hhdm virt address
+ * @return addr to a physical address but in the hhdm (higher half direct mapping) so the kernel
+ * could easily read and write for it cause it is already mapped to memory
+ */
+void* pmm_alloc_page_hhdm(void);
+void* pmm_alloc_pages_hhdm(size_t count);
 
 void pmm_free_page(void* physical_addr);
 void pmm_free_pages(void* physical_addr, size_t count);
--- kernel/mm/pmm/pmm_bitmap.c
+++ kernel/mm/pmm/pmm_bitmap.c
@@ -6,7 +6,6 @@
 
 #include "kernel/include/pmm.h"
 
-#define PAGE_SIZE 0x1000
 #define BITS_PER_BYTE 8
 #define DIV_ROUND_UP(x, align) (((x) + (align) - 1) / (align))
 static inline size_t get_ram_max_size(volatile struct limine_memmap_response* memmap_response);
@@ -17,11 +16,13 @@ static inline void set_page_as_unused(void* page_addr);
 static void mark_region_as_used(struct limine_memmap_entry* entry);
 static void mark_region_as_usable(struct limine_memmap_entry* entry);
 
-static struct tz_bitmap s_bitmap;
+static struct tz_bitmap g_bitmap;
+static size_t g_hhdm_offset;
 void init_pmm(volatile struct limine_memmap_response* memmap_response,
 			  volatile struct limine_hhdm_response* hhdm_response) {
 	KERNEL_ASSERT(memmap_response != NULL, "Limine memmap response is NULL cannot initialize PMM");
 	KERNEL_ASSERT(hhdm_response != NULL, "Limine HHDM response is NULL cannot initialize PMM");
+	g_hhdm_offset = hhdm_response->offset;
 
 	// Identifiing a region for the bitmap:
 	size_t systems_physical_ram_size = get_ram_max_size(memmap_response);
@@ -31,12 +32,12 @@ void init_pmm(volatile struct limine_memmap_response* memmap_response,
 	struct limine_memmap_entry* bitmap_region = memmap_response->entries[bitmap_region_index];
 
 	// Creating the bitmap:
-	uint64_t bits_needed_for_bitmap = systems_physical_ram_size / PAGE_SIZE;
+	uint64_t bits_needed_for_bitmap = systems_physical_ram_size / REGULAR_PAGE_SIZE;
 	uint64_t bytes_needed_for_bitmap = DIV_ROUND_UP(bits_needed_for_bitmap, BITS_PER_BYTE);
-	uint64_t pages_needed_for_bitmap = DIV_ROUND_UP(bytes_needed_for_bitmap, PAGE_SIZE);
-	s_bitmap =
+	uint64_t pages_needed_for_bitmap = DIV_ROUND_UP(bytes_needed_for_bitmap, REGULAR_PAGE_SIZE);
+	g_bitmap =
 		bitmap_create((void*)bitmap_region->base + hhdm_response->offset, bits_needed_for_bitmap);
-	memset(s_bitmap.bitmap, 0xFF, bytes_needed_for_bitmap);
+	memset(g_bitmap.bitmap, 0xFF, bytes_needed_for_bitmap);
 
 	// Marking all the safe usable regions:
 	for (uint32_t i = 0; i < memmap_response->entry_count; i++) {
@@ -46,18 +47,18 @@ void init_pmm(volatile struct limine_memmap_response* memmap_response,
 	}
 
 	for (uint64_t i = 0; i < pages_needed_for_bitmap; i++) {
-		set_page_as_used((void*)(bitmap_region->base + (i * PAGE_SIZE)));
+		set_page_as_used((void*)(bitmap_region->base + (i * REGULAR_PAGE_SIZE)));
 	}
-	printk("initialized PMM Bitmap! Bitmap located at: 0x%x\n", s_bitmap.bitmap);
+	printk("initialized PMM Bitmap! Bitmap located at: 0x%x\n", g_bitmap.bitmap);
 }
 
 void* pmm_alloc_page(void) {
-	for (uint64_t i = 0; i < s_bitmap.bit_count; i++) {
-		if (bitmap_get_bit_value(&s_bitmap, i)) {
+	for (uint64_t i = 0; i < g_bitmap.bit_count; i++) {
+		if (bitmap_get_bit_value(&g_bitmap, i)) {
 			continue;
 		}
 
-		void* page_addr = (void*)(i * PAGE_SIZE);
+		void* page_addr = (void*)(i * REGULAR_PAGE_SIZE);
 		set_page_as_used(page_addr);
 		return page_addr;
 	}
@@ -68,10 +69,10 @@ void* pmm_alloc_pages(size_t count) {
 	KERNEL_ASSERT(count != 0, "PMM can't allocate 0 pages");
 
 	bool found_range = true;
-	for (uint64_t i = 0; i <= s_bitmap.bit_count - count; i++) {
+	for (uint64_t i = 0; i <= g_bitmap.bit_count - count; i++) {
 		found_range = true;
 		for (uint64_t j = i; j < i + count; j++) {
-			if (bitmap_get_bit_value(&s_bitmap, j)) {
+			if (bitmap_get_bit_value(&g_bitmap, j)) {
 				found_range = false;
 				i = j;
 				break;
@@ -82,18 +83,33 @@ void* pmm_alloc_pages(size_t count) {
 		}
 
 		for (uint64_t j = i; j < i + count; j++) {
-			set_page_as_used((void*)(j * PAGE_SIZE));
+			set_page_as_used((void*)(j * REGULAR_PAGE_SIZE));
 		}
-		return (void*)(i * PAGE_SIZE);
+		return (void*)(i * REGULAR_PAGE_SIZE);
 	}
 
 	return NULL;
 }
 
+void* pmm_alloc_page_hhdm(void) {
+	void* page = pmm_alloc_page();
+	if (page) {
+		return page + g_hhdm_offset;
+	}
+	return page;
+}
+void* pmm_alloc_pages_hhdm(size_t count) {
+	void* pages = pmm_alloc_pages(count);
+	if (pages) {
+		return pages + g_hhdm_offset;
+	}
+	return pages;
+}
+
 void pmm_free_page(void* physical_addr) { set_page_as_unused(physical_addr); }
 void pmm_free_pages(void* physical_addr, size_t count) {
 	for (uint64_t i = 0; i < count; i++) {
-		set_page_as_unused((void*)((uint64_t)physical_addr + (i * PAGE_SIZE)));
+		set_page_as_unused((void*)((uint64_t)physical_addr + (i * REGULAR_PAGE_SIZE)));
 	}
 }
 
@@ -111,7 +127,7 @@ static inline size_t get_ram_max_size(volatile struct limine_memmap_response* me
 
 static inline int64_t get_bitmap_region(volatile struct limine_memmap_response* memmap_response,
 										size_t physical_ram_size) {
-	uint64_t bits_needed_for_bitmap = physical_ram_size / PAGE_SIZE;
+	uint64_t bits_needed_for_bitmap = physical_ram_size / REGULAR_PAGE_SIZE;
 	uint64_t bytes_needed_for_bitmap = DIV_ROUND_UP(bits_needed_for_bitmap, BITS_PER_BYTE);
 
 	for (uint32_t i = 0; i < memmap_response->entry_count; i++) {
@@ -128,23 +144,25 @@ static inline int64_t get_bitmap_region(volatile struct limine_memmap_response* 
 }
 
 static inline void set_page_as_used(void* page_addr) {
-	KERNEL_ASSERT(bitmap_set_bit_value(&s_bitmap, (uint64_t)page_addr / PAGE_SIZE, true) != -1,
-				  "PMM bitmap bit set out of bounds");
+	KERNEL_ASSERT(
+		bitmap_set_bit_value(&g_bitmap, (uint64_t)page_addr / REGULAR_PAGE_SIZE, true) != -1,
+		"PMM bitmap bit set out of bounds");
 }
 
 static inline void set_page_as_unused(void* page_addr) {
-	KERNEL_ASSERT(bitmap_set_bit_value(&s_bitmap, (uint64_t)page_addr / PAGE_SIZE, false) != -1,
-				  "PMM bitmap bit set out of bounds");
+	KERNEL_ASSERT(
+		bitmap_set_bit_value(&g_bitmap, (uint64_t)page_addr / REGULAR_PAGE_SIZE, false) != -1,
+		"PMM bitmap bit set out of bounds");
 }
 
 static void mark_region_as_used(struct limine_memmap_entry* entry) {
-	for (uint64_t i = 0; i < entry->length / PAGE_SIZE; i++) {
-		set_page_as_used((void*)(entry->base + (i * PAGE_SIZE)));
+	for (uint64_t i = 0; i < entry->length / REGULAR_PAGE_SIZE; i++) {
+		set_page_as_used((void*)(entry->base + (i * REGULAR_PAGE_SIZE)));
 	}
 }
 
 static void mark_region_as_usable(struct limine_memmap_entry* entry) {
-	for (uint64_t i = 0; i < entry->length / PAGE_SIZE; i++) {
-		set_page_as_unused((void*)(entry->base + (i * PAGE_SIZE)));
+	for (uint64_t i = 0; i < entry->length / REGULAR_PAGE_SIZE; i++) {
+		set_page_as_unused((void*)(entry->base + (i * REGULAR_PAGE_SIZE)));
 	}
 }
