c88c0e08d3594eb41ff3aee8a1c64787e6b8c2e7
Nir Kriemer <nirmot327@gmail.com>
2026-04-08 00:15:27 +0300

    (feature)[libc_tz]: Add strncmp function.

--- include/strings.h
+++ include/strings.h
@@ -3,4 +3,5 @@
 
 uint64_t strlen(char* str);
 int64_t strcmp(const char* str1, const char* str2);
+int64_t strncmp(const char* str1, const char* str2, size_t n);
 void string_reverse(char* str);
--- utils/libc_tz/strings.c
+++ utils/libc_tz/strings.c
@@ -19,6 +19,20 @@ int64_t strcmp(const char* str1, const char* str2) {
 	return *(const unsigned char*)str1 - *(const unsigned char*)str2;
 }
 
+int64_t strncmp(const char* str1, const char* str2, size_t n) {
+	for (size_t i = 0; i < n; i++) {
+		if (str1[i] != str2[i]) {
+			return (unsigned char)str1[i] - (unsigned char)str2[i];
+		}
+
+		if (str1[i] == '\0') {
+			return 0;
+		}
+	}
+
+	return 0;
+}
+
 void string_reverse(char* str) {
 	int str_len = (int)strlen(str);
 	char temp = 0;
