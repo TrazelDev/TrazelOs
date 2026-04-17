a3b4d79f337ce9c438ca07fe3ea6c61d7bda5490
Nir Kriemer <nirmot327@gmail.com>
2026-04-18 01:00:45 +0300

    (fix)[hardware interrupts]: Fix hardware interrupt function pointers.
    * There was an issue where when I would get an index for the a certain hardware interrupt idt index I indexed in the wrong place to the wrong place in the array.

--- kernel/interrupts/hardware_intrrupts/hardware_intrrupts.c
+++ kernel/interrupts/hardware_intrrupts/hardware_intrrupts.c
@@ -7,8 +7,8 @@ static void (*g_hardware_interrupt_handlers[HARDWARE_INTERRUPT_COUNT])(
 	struct interrupt_info* info) = {NULL};
 
 void hardware_interrupts_isr_central_handler(struct interrupt_info* info) {
-	if (g_hardware_interrupt_handlers[info->interrupt_index]) {
-		g_hardware_interrupt_handlers[info->interrupt_index](info);
+	if (g_hardware_interrupt_handlers[info->interrupt_index - CPU_EXCEPTION_INTERRUPT_COUNT]) {
+		g_hardware_interrupt_handlers[info->interrupt_index - CPU_EXCEPTION_INTERRUPT_COUNT](info);
 		return;
 	}
 
@@ -18,7 +18,7 @@ void hardware_interrupts_isr_central_handler(struct interrupt_info* info) {
 
 void set_hardware_interrupt_handler(uint8_t index, void (*handler)(struct interrupt_info* info)) {
 	KERNEL_ASSERT(index >= CPU_EXCEPTION_INTERRUPT_COUNT, "Invalid hardware interrupt index");
-	g_hardware_interrupt_handlers[index] = handler;
+	g_hardware_interrupt_handlers[index - CPU_EXCEPTION_INTERRUPT_COUNT] = handler;
 }
 
 uint8_t get_free_interrupt_desc(void) {
