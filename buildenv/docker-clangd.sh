9b5a071f9d154dfd559e84193153a24105160c73
Nir Kriemer <nirmot327@gmail.com>
2026-09-17 17:00:02 +0300

    (feature)[clangd]: Add inlay hints(parameter names)

--- buildenv/docker-clangd.sh
+++ buildenv/docker-clangd.sh
@@ -3,6 +3,6 @@
 HOST_PATH="$(pwd)"
 CONTAINER_PATH="/mnt/TrazelOs"
 
-docker exec -i trazelos-dev clangd \
+docker exec -i trazelos-dev clangd-15 \
     --path-mappings="${HOST_PATH}=${CONTAINER_PATH}" \
     "$@"
